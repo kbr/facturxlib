df1952495612bdacdb50edaca6ce398902cf5d55
Klaus Bremer <bremer@bremer-media.com>
2024-05-26 10:27:07 +0200

    more trade partys

--- facturxlib/transaction/tradesettlement.py
+++ facturxlib/transaction/tradesettlement.py
@@ -9,6 +9,7 @@ from typing import Optional, Sequence
 from ..nodes.common import (
     cii_node,
     AllowanceTotalAmount,
+    BaseTradeParty,
     BasisAmount,
     CalculatedAmount,
     CategoryCode,
@@ -22,15 +23,53 @@ from ..nodes.common import (
     TaxTotalAmount,
     TotalPrepaidAmount,
     TypeCode,
+    ValueClass,
 )
 
 
+@cii_node("udt")
+class CreditorReferenceID(ValueClass):
+    """Bank assigned creditor identifier."""
+
+
+@cii_node("udt")
+class InvoiceIssuerReference(ValueClass):
+    """Remittance Seller reference number."""
+
+
 @cii_node("qdt")
-class InvoiceCurrencyCode:
+class InvoiceCurrencyCode(ValueClass):
     """represents a tag specifying a currency code like "EUR"."""
 
-    def __init__(self, value):
-        self._value = value
+
+@cii_node("ram")
+class InvoicerTradeParty(BaseTradeParty):
+    """InvoicerTradeParty"""
+
+@cii_node("ram")
+class InvoiceeTradeParty(BaseTradeParty):
+    """Detailed information about the deviating invoice recipient"""
+
+@cii_node("ram")
+class PayeeTradeParty(BaseTradeParty):
+    """Detailed contact information about the Payee"""
+
+@cii_node("ram")
+class PayerTradeParty(BaseTradeParty):
+    """PayerTradeParty"""
+
+
+
+
+
+@cii_node("udt")
+class PaymentReference(ValueClass):
+    """Remittance information."""
+
+
+@cii_node("qdt")
+class TaxCurrencyCode(ValueClass):
+    """VAT accounting currency code"""
 
 
 @dataclass
@@ -120,8 +159,37 @@ class ApplicableHeaderTradeSettlement:
     `applicable_trade_tax`: Sequence of VAT Breakdowns (ApplicableTradeTax)
     `specified_trade_settlement_header_monetary_summation`:
             Detailed information about document totals
+
+    optional:
+    `creditor_reference_id`: Unique banking reference identifier of the
+            payee or seller assigned by the payee or seller bank.
+            Creditor-ID number for SEPA
+    `payment_reference`: A textual value used to establish a link between
+            the payment and the invoice, issued by the seller. This
+            information element helps the seller to assign an incoming
+            payment to the relevant payment process.
+    `tax_currency_code`: The VAT total amount expressed in the accounting
+            currency accepted or required in the country of the seller.
+    `invoice_issuer_reference`: Given seller reference number for routing
+            purposes after biliteral agreement
+    `invoicer_trade_party`: InvoicerTradeParty
+    `invoicee_trade_party`: Detailed information about the deviating invoice recipient
+    `payee_trade_party`: Detailed contact information about the Payee
+    `payer_trade_party`: PayerTradeParty
     """
 
     invoice_currency_code: InvoiceCurrencyCode
     applicable_trade_taxes: Sequence[ApplicableTradeTax]
     specified_trade_settlement_header_monetary_summation: SpecifiedTradeSettlementHeaderMonetarySummation
+    creditor_reference_id: Optional[CreditorReferenceID] = None
+    payment_reference: Optional[PaymentReference] = None
+    tax_currency_code: Optional[TaxCurrencyCode] = None
+    invoice_issuer_reference: Optional[InvoiceIssuerReference] = None
+    invoicer_trade_party: Optional[InvoicerTradeParty] = None
+    invoicee_trade_party: Optional[InvoiceeTradeParty] = None
+    payee_trade_party: Optional[PayeeTradeParty] = None
+    payer_trade_party: Optional[PayerTradeParty] = None
+
+
+
+
